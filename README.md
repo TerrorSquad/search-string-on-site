9b04fbab0091fba9de1fa1f02b077a8cf284c653
Goran Ninkovic <warhawk@hotmail.rs>
2020-10-12 23:49:18 +0200

    feat: rename to suos

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-ssos
+suos
 ====
 
 Search String on Site
 
 [![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
-[![Version](https://img.shields.io/npm/v/ssos.svg)](https://npmjs.org/package/ssos)
-[![Downloads/week](https://img.shields.io/npm/dw/ssos.svg)](https://npmjs.org/package/ssos)
-[![License](https://img.shields.io/npm/l/ssos.svg)](https://github.com/TerrorSquad/ssos/blob/master/package.json)
+[![Version](https://img.shields.io/npm/v/suos.svg)](https://npmjs.org/package/suos)
+[![Downloads/week](https://img.shields.io/npm/dw/suos.svg)](https://npmjs.org/package/suos)
+[![License](https://img.shields.io/npm/l/suos.svg)](https://github.com/TerrorSquad/suos/blob/master/package.json)
 
 <!-- toc -->
 * [Usage](#usage)
@@ -15,14 +15,14 @@ Search String on Site
 # Usage
 <!-- usage -->
 ```sh-session
-$ npm install -g ssos
-$ ssos COMMAND
+$ npm install -g suos
+$ suos COMMAND
 running command...
-$ ssos (-v|--version|version)
-ssos/1.0.1 linux-x64 node-v14.9.0
-$ ssos --help [COMMAND]
+$ suos (-v|--version|version)
+suos/1.0.1 linux-x64 node-v14.9.0
+$ suos --help [COMMAND]
 USAGE
-  $ ssos COMMAND
+  $ suos COMMAND
 ...
 ```
 <!-- usagestop -->
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,23 @@ import * as fs from 'fs'
 import isTextFile from './utils/textFileType'
 import xml2Json from './utils/xmlToJson'
 import got from 'got'
+const workerpool=require('workerpool');
+const pool=workerpool.pool();
 
-class Ssos extends Command {
-  static description='describe the command here'
+class Suos extends Command {
+  static description='Searches all pages of website based on a sitemap and search string'
 
   static flags={
+    allUrls: flags.boolean({ name: 'allUrls', char: 'a', description: "Allow running command without passing a url to find", default: false }),
+    printSearchRegex: flags.boolean({ name: 'printSearchRegex', char: 'p', description: "Print search regex", default: false }),
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
   }
 
-  static args=[{ name: 'file' }]
+  static args=[{ name: 'file' }, { name: 'urlToFind' }]
 
   async run() {
-    const { args, flags }=this.parse(Ssos)
+    const { args, flags }=this.parse(Suos)
     const file=args.file
     if (!file) {
       throw new Error(`You must specify a file`)
@@ -23,38 +27,58 @@ class Ssos extends Command {
     if (!isTextFile(file)) {
       throw new Error(`You must provide a text file.`)
     }
+    let urlToFind=""
+    if (!flags.allUrls) {
+      urlToFind=args.urlToFind
+      if (!urlToFind)
+        throw new Error(`You must provide a URL to search for`)
+    }
 
     const xmlData=fs.readFileSync(file, "utf8")
     const parsedXml=xml2Json(xmlData);
 
-    const listOfUrls=new Set()
+    const listOfUrls=new Set<string>()
     parsedXml.urlset.url.forEach((el: any) => {
       listOfUrls.add(el.loc);
     })
-
-
-
-    const bodyRegex=/(<body>[\s\W\w]*?<\/body>)/g
-    const hrefRegex=/(?:href="(?<url>http(?:s)?:\/\/(?:www\.)?(?:admin).*?)")/gm;
-
-    listOfUrls.forEach((url: any) => {
-      got(url).then((res) => {
-        const htmlBody=res?.body?.match(bodyRegex)?.[0];
-        const hrefsMatches=htmlBody?.matchAll(hrefRegex)||[]
-        const hrefs=Array.from(hrefsMatches, (regexMatch: any) => {
-          return regexMatch.groups.url
-        })
-
-        if (hrefs.length) {
-          console.log(`Result found on URL: ${url}`);
-          console.dir(new Set(hrefs))
-        }
-      }).catch(err => {
-        console.log(err);
-      });
-    })
-
+    const searchRegex=getSearchRegex(urlToFind)
+    if (flags.printSearchRegex) {
+      console.log(searchRegex)
+    }
+    await findUrlsOnPage(listOfUrls, searchRegex)
   }
 }
 
-export = Ssos
+const findUrlsOnPage=async (listOfUrls: Set<string>, searchRegex: RegExp) => {
+  const bodyRegex=/(<body>[\s\W\w]*?<\/body>)/g
+  const scriptRegex=/(<script>[\s\W\w]*?<\/script>)/g
+
+
+  listOfUrls.forEach(async (url) => {
+    const res=await got(url)
+    const htmlBody=res?.body?.match(bodyRegex)?.[0];
+    const cleanBody=htmlBody?.replace(scriptRegex, "")
+    const hrefsMatches=cleanBody?.matchAll(searchRegex)||[]
+    const hrefs=Array.from(hrefsMatches, (regexMatch: any) => {
+      return regexMatch.groups.url
+    })
+
+    if (hrefs.length) {
+      const resultSet=new Set(hrefs);
+      for (const result of resultSet) {
+        console.log(`${url},${result}`)
+      }
+    }
+  })
+
+}
+
+const getSearchRegex=(urlToFind: string) => {
+  const regexStart="(?:href=\""
+  const regexUrl=new RegExp("(?<url>.*?"+urlToFind+".*?)")
+  const regexEnd="\")"
+  const hrefRegex=new RegExp(regexStart+regexUrl.source+regexEnd, 'gm')
+  return hrefRegex
+}
+
+export = Suos
